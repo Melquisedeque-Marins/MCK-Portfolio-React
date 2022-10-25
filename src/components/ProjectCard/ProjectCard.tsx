f7c26f1214889da61ad9031a6ee8ade6bcf62617
Melquisedeque <melck_junior@hotmail.com>
2022-10-25 16:02:01 +0100

    add images in about screen

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,14 +1,13 @@
-
 import './ProjectCard.css';
-
 
 type props = {
     src: string;
     title: string;
     description: string;
+    link: string;
 }
 
-const ProjectCard = ({src, title, description}: props) => {
+const ProjectCard = ({src, title, description, link}: props) => {
     return (
         <div className="card-container">
              <div className="content-card">
@@ -18,6 +17,7 @@ const ProjectCard = ({src, title, description}: props) => {
                     <div className="text-card">
                         <h1>{title}</h1>
                         <p>{description}</p>
+                        <a href={link} target="_blank">Confira o código no Github</a>
                     </div>
 
                 </div>
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,11 +1,11 @@
-
+import Prof from '../../assets/img/about.jpg'
 import './About.css';
 
 const About = () => {
     return (
         <div className="container-about">
-            <div className="content">
-               
+            <div className="content-about">
+                <img src={Prof} alt="" />
             </div>
         </div>
     )
