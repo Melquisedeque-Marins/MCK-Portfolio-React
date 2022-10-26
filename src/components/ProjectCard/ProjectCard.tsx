d56e99e6fa39b0944d39c17cba0a06610e9bbfea
Melquisedeque <melck_junior@hotmail.com>
2022-10-26 18:38:58 +0100

    Slider banner

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -7,27 +7,27 @@ type Props = {
     link: string;
 }
 
-const ProjectCard = (props : Props) => {
+const ProjectCard = (props: Props) => {
     return (
         <div className="card-container">
-             <div className="content-card">
-                    <div className="carro">
-                        <div className="img-card">
-                            {
-                                props.src.map((s: string) => (
-                                    <img src={s} alt="project image" />
-                                )
-                                )
-                            }
-                        </div>
+            <div className="content-card">
+                <div className="carro">
+                    <div className="img-card">
+                        {
+                            props.src.map((s: string) => (
+                                <img src={s} alt="project image" />
+                            )
+                            )
+                        }
                     </div>
-                    <div className="text-card">
-                        <h1>{props.title}</h1>
-                        <p>{props.description}</p>
-                        <a href={props.link} target="_blank">Confira o código no Github</a>
-                    </div>
-
                 </div>
+                <div className="text-card">
+                    <h1>{props.title}</h1>
+                    <p>{props.description}</p>
+                    <a href={props.link} target="_blank">Confira o código no Github</a>
+                </div>
+
+            </div>
         </div>
     )
 }
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,6 +1,8 @@
 import './Projects.css';
 import ProjectCard from '../../components/ProjectCard/ProjectCard';
 import Cat_index from '../../assets/img/MCK-catalog-index.png'
+import Cat_catalog from '../../assets/img/MCK-catalog-catalog.png'
+import Cat_details from '../../assets/img/MCK-catalog-details.png'
 import Car_index from '../../assets/img/MCK-car-index.png'
 import Mck from '../../assets/img/Home-screen.png'
 import Api from '../../assets/img/api_logo.png'
@@ -13,9 +15,9 @@ const Projects = () => {
                     <div className="card-title">
                         <h1>Projetos Full stack Spring React</h1>
                     </div>
-                    <ProjectCard src={[Cat_index, Car_index]} title="MCK Catalog" description='Projeto executado no Bootcamp spring react da DevSuperior.
+                    <ProjectCard src={[Cat_index, Cat_catalog, Cat_details, Cat_index]} title="MCK Catalog" description='Projeto executado no Bootcamp spring react da DevSuperior.
                     Um catálogo de proutos que se comunica com uma rest api em Java e exibe uma lista de produtos cadastros, bem como preços e informações detalhadas de cada item.
-                    possui também área de administrador e operador.' link='https://github.com/Melquisedeque-Marins/MCK-Clinic-Thymeleaf'/>
+                    possui também área de administrador e operador.' link='https://github.com/Melquisedeque-Marins/MCK-Catalog'/>
                     <ProjectCard src={[Car_index]} title="MCK Car" description='Projeto executado no Bootcamp spring react da DevSuperior.
                     Um projeto simples de catálogo de carros para desenvolver a pratica de habilidades como: capaciade de desenhar layouts responsivos, navegação entre paginas, rotas, utilização do framework Bootstrap, etc.' link='https://github.com/Melquisedeque-Marins/MCK-Clinic-Thymeleaf'/>
                 </div>
@@ -25,7 +27,7 @@ const Projects = () => {
                         <h1>Projetos Pessoais</h1>
                     </div>
                     <ProjectCard src={[Api]} title="MCK Clinic - Rest API" description='API Rest desenvolvida em Java com Spring Boot utilizando boas práticas de programação.' link='https://github.com/Melquisedeque-Marins/MCK-Clinic-Thymeleaf'/>
-                    <ProjectCard src={[Mck]} title="MCK Clinic" description='testando props em um componente' link='https://github.com/Melquisedeque-Marins/MCK-Clinic-Thymeleaf#readme'/>
+                    <ProjectCard src={[Mck]} title="MCK Clinic" description="" link='https://github.com/Melquisedeque-Marins/MCK-Clinic-Thymeleaf#readme'/>
                 </div>
             </div>
         </div>
